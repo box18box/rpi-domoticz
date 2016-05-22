eef0746b9c2762d2c11228956d84320cb31a79ea
Sébastien Laporte <seb_laporte_33@yahoo.com>
2016-05-22 09:56:41 +0200

    Change configuration

--- rpi-domoticz-start.sh
+++ rpi-domoticz-start.sh
@@ -2,9 +2,9 @@
 docker run -d \
   -p 8080:8080 \
   --name=rpi-domoticz \
-  -v /root/rpi-domoticz/config:/config \
   -v /etc/localtime:/etc/localtime:ro \
-  -v /root/rpi-domoticz/scripts/lua:/domoticz/scripts/lua \
-  -v /root/rpi-domoticz/scripts/actions:/domoticz/scripts/actions \
+  -v /root/persistent/rpi-domoticz/config:/config \
+  -v /root/persistent/rpi-domoticz/scripts/lua:/domoticz/scripts/lua \
+  -v /root/persistent/rpi-domoticz/scripts/actions:/domoticz/scripts/actions \
   --privileged \
   rpi-domoticz-v1
